b3705a42b810ee97ef243f279255a7aba16b3292
FARUK2059 <omarfaruk34552@gmail.com>
2026-01-24 20:52:44 +0600

    add theme toggle functionality and integrate next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "@/components/shared/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,15 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* <Providers> */}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <Toaster richColors position="top-right" />
           {children}
+        </ThemeProvider>
         {/* </Providers> */}
       </body>
     </html>
