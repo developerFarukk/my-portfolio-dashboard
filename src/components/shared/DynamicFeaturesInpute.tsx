5a115367e05efed091d573e373a7b0d5eadd1e8c
FARUK2059 <omarfaruk34552@gmail.com>
2026-01-27 01:21:28 +0600

    feat: enhance DynamicFeaturesInput component with improved layout and styling; add feature title and description handling

--- src/components/shared/DynamicFeaturesInpute.tsx
+++ src/components/shared/DynamicFeaturesInpute.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Plus, Minus } from "lucide-react";
+import { Plus, Minus, StarIcon, ChevronDown } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { FormLabel } from "@/components/ui/form";
 import { motion } from "framer-motion";
+import { Button } from "../ui/button";
+import { Textarea } from "../ui/textarea";
 
 /* ================= TYPES ================= */
 
@@ -30,7 +32,7 @@ interface DynamicFeaturesInputProps {
 export const DynamicFeaturesInput = ({
   value = [],
   onChange,
-  label = "Project Features",
+  label = "Project Features 1",
 }: DynamicFeaturesInputProps) => {
   const [features, setFeatures] = useState<TProjectFeature[]>([
     {
@@ -103,61 +105,80 @@ export const DynamicFeaturesInput = ({
   return (
     <div className="space-y-4">
       {/* Header */}
-      <div className="flex justify-between items-center">
-        <FormLabel className="font-semibold italic">
-          {label} <span className="text-xs text-red-600">(Optional)</span>
-        </FormLabel>
-
+      <div className="flex justify-end">
         <motion.button
           type="button"
           onClick={addFeature}
           whileTap={{ scale: 1.15 }}
+          className="dark:bg-slate-700 bg-gray-400/30 hover:bg-slate-400/50 hover:dark:bg-slate-700/80 text-black dark:text-white text-md font-semibold rounded-3xl px-3 py-1"
         >
-          <Plus size={20} />
+          <div className="flex items-center gap-2 text-lg font-semibold italic">
+            <Plus size={20} />
+            <h2>Add Feature</h2>
+          </div>
         </motion.button>
       </div>
 
       {/* Features */}
       {features.map((feature, fIdx) => (
         <div key={fIdx} className="border rounded-md p-3 space-y-3 bg-muted/30">
+          <FormLabel className="font-semibold italic">
+            {`Project Feature ${fIdx + 1}`}{" "}
+            <span className="text-xs text-red-600">(Optional)</span>
+          </FormLabel>
           {/* Feature Title */}
-          <Input
-            placeholder={`Feature ${fIdx + 1} Title`}
-            value={feature.pFeatureTitle ?? ""}
-            onChange={(e) => updateFeatureTitle(fIdx, e.target.value)}
-          />
+          <div className="flex gap-2 justify-start items-center">
+            <div className="lg:w-24 w-16 italic">
+              <h2>Title {`${fIdx + 1}`} :</h2>
+            </div>
+            <Input
+              placeholder={`Inpute feature title ${fIdx + 1}`}
+              value={feature.pFeatureTitle ?? ""}
+              onChange={(e) => updateFeatureTitle(fIdx, e.target.value)}
+            />
+          </div>
 
           {/* Descriptions */}
-          <div className="space-y-2">
+          <div className="space-y-2 border p-2 rounded-md bg-fuchsia-100 dark:bg-cyan-950">
+            <div className="text-center italic">
+              <h2 className="text-md font-medium p-1 mb-2">Descriptions</h2>
+            </div>
             {feature.pFeatureDescriptions?.map((desc, dIdx) => (
-              <div key={dIdx} className="flex gap-2 items-center">
-                <Input
-                  placeholder="Feature description"
-                  value={desc}
-                  onChange={(e) =>
-                    updateDescription(fIdx, dIdx, e.target.value)
-                  }
-                />
+              <div key={dIdx} className="">
+                <div className="flex gap-2 items-center">
+                  <div className=" italic w-6">
+                    <h2 className="border rounded-full p-2 border-blue-200 dark:border-amber-900">{` ${fIdx + 1}`}</h2>
+                  </div>
+                  <Textarea
+                    placeholder={`Inpute feature description ${fIdx + 1}`}
+                    value={desc}
+                    onChange={(e) =>
+                      updateDescription(fIdx, dIdx, e.target.value)
+                    }
+                  />
 
-                {dIdx > 0 && (
-                  <motion.button
-                    type="button"
-                    onClick={() => removeDescription(fIdx, dIdx)}
-                    whileTap={{ scale: 1.2 }}
-                  >
-                    <Minus size={18} />
-                  </motion.button>
-                )}
+                  {dIdx > 0 && (
+                    <motion.button
+                      type="button"
+                      onClick={() => removeDescription(fIdx, dIdx)}
+                      whileTap={{ scale: 1.2 }}
+                    >
+                      <Minus size={18} />
+                    </motion.button>
+                  )}
+                </div>
               </div>
             ))}
 
-            <button
-              type="button"
-              onClick={() => addDescription(fIdx)}
-              className="text-xs text-blue-600"
-            >
-              + Add description
-            </button>
+            <div className="flex justify-end mt-4">
+              <button
+                type="button"
+                onClick={() => addDescription(fIdx)}
+                className="text-xs dark:text-blue-200 border p-1 rounded-2xl border-amber-800"
+              >
+                + Add description
+              </button>
+            </div>
           </div>
 
           {/* Remove Feature */}
